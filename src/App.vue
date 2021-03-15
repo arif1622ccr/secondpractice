3cae314206451aa1275e35df1867b8375240a312
arif1622ccr <arif1622ccr@gmail.com>
2021-03-15 22:01:05 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/black.gif">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test msg="This is test"/>
+    <Contact />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/test.vue'
+import Contact from './components/contact.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Test,
+    Contact
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/contact.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="contact">
+        
+         FirstName : <input type = "text" v-model = "firstname" /> <br/><br/>
+         LastName : <input type = "text" v-model = "lastname"/> <br/><br/>
+         <h1>My name is {{firstname}} {{lastname}}</h1>
+         <h1>Using computed method : {{getfullname}}</h1>
+         <h1>Using method : {{namebymethod()}}</h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Contact',
+    data: function(){
+        return{
+            message: "This is contact page",
+            firstname: "",
+            lastname: ""
+        }
+    },
+    computed: {
+        getfullname: function(){
+            return this.firstname + " "+ this.lastname
+        }
+    },
+    methods: {
+        namebymethod: function(){
+            return this.firstname+ " "+ this.lastname
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
